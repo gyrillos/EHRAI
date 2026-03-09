3892b9b40ede1e44b22763f4e5ecf0f68c8ec11c
Kyrillos Abdelshaheed <kyrillos.abdelshaheed.22@cnu.edu>
2026-03-09 19:14:16 -0400

    Spring break push

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import "./components/Layout.css";
+
+import Upload from "./pages/Upload";
+import Review from "./pages/Review";
+import Confirm from "./pages/Confirm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+
+    <div className="layout">
+      <Navbar></Navbar>
+      <div className="main-area">
+        <Header></Header>
+        <div className="body">
+        <Routes>
+          <Route path="/" element={<Upload/>}/>
+          <Route path="/upload" element={<Upload/>}/>
+          <Route path="/review" element={<Review/>}/>
+          <Route path="/confirm" element={<Confirm/>}/>
+        </Routes>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>  
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
